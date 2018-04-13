560a0349d8e08ed6c350bf2f1b876f9b517d217c
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-04-13 12:18:00 +0300

    :wrench: add graphql initializer

---
+++ config/initializers/graphql.rb
@@ -0,0 +1,38 @@
+require 'graphql/client'
+require 'graphql/client/http'
+
+module Graphql
+  class Common
+    class << self
+      def build_client(endpoint)
+        http = GraphQL::Client::HTTP.new endpoint
+        schema = GraphQL::Client.load_schema http
+        GraphQL::Client.new schema: schema, execute: http
+      end
+
+      def client
+        make_client
+      end
+
+      def make_client
+        raise 'Not implemented yet!'
+      end
+    end
+  end
+
+  class Comments < Common
+    class << self
+      def make_client
+        build_client Rails.configuration.comments_endpoint
+      end
+    end
+  end
+
+  class Posts < Common
+    class << self
+      def make_client
+        build_client Rails.configuration.posts_endpoint
+      end
+    end
+  end
+end
